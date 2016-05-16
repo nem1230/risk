dde67a1fd6fa8c43cfdcebfe38d5dd802deb8b31
Nick N <n.nemetz1230@gmail.com>
2016-05-16 09:37:32 -0700

    initial commit map has been completely imaged and objects have been created in app.js using jquery

---
+++ app.js
@@ -0,0 +1,60 @@
+console.log($('img[usemap]'));
+$('img[usemap]').maphilight()
+
+
+
+
+  var $continents = {
+    $australia: $('.australia'),
+    $asia: $('.asia'),
+    $europe: $('.europe'),
+    $africa: $('.africa'),
+    $southAmerica: $('.southAmerica'),
+    $northAmerica: $('.northAmerica')
+  }
+
+
+  var territories = {
+    $indonesia: $('.indonesia'),
+    newGuinea: $('.newGuinea'),
+    westernAus: $('.westernAus'),
+    easternAus: $('.easternAus'),
+    siam: $('.siam'),
+    india: $('.india'),
+    china: $('.china'),
+    middleEast: $('.middleEast'),
+    afghanistan: $('.afghanistan'),
+    ural: $('.ural'),
+    siberia: $('.siberia'),
+    yakutsk: $('.yakutsk'),
+    irkutsk: $('.irkutsk'),
+    mongolia: $('.mongolia'),
+    japan: $('.japan'),
+    kamchatka: $('.kamchatka'),
+    ukraine: $('.ukraine'),
+    scandanavia: $('.scandanavia'),
+    iceland: $('.iceland'),
+    greatBritain: $('.greatBritain'),
+    northernEurope: $('.northernEurope'),
+    westernEurope: $('.westernEurope'),
+    southernEurope: $('.southernEurope'),
+    northAfrica: $('.northAfrica'),
+    egypt: $('.egypt'),
+    eastAfrica: $('.eastAfrica'),
+    congo: $('.congo'),
+    southAfrica: $('.southAfrica'),
+    madagascar: $('.madagascar'),
+    argentina: $('.argentina'),
+    peru: $('.peru'),
+    brazil: $('.brazil'),
+    venezuela: $('.venezuela'),
+    centralAmerica: $('.centralAmerica'),
+    easternUnitedStates: $('.easternUnitedStates'),
+    westernUnitedStates: $('.westernUnitedStates'),
+    alberta: $('.alberta'),
+    ontario: $('.ontario'),
+    quebec: $('.quebec'),
+    greenland: $('.greenland'),
+    northwestTerritory: $('.northwestTerritory'),
+    alaska: $('.alaska')
+  }

f05ecb6730babf081920c1fe41c906ea1436b717
Nick N <n.nemetz1230@gmail.com>
2016-05-16 12:27:05 -0700

    added a switch turns function that isn't finished yet. added die variables that are assigned a random number. these function and are pushed to attacker and defender arrays on click of a button. Also on click, the function of grabbing the highest 2 numbers of the attacker array and pushing them to a new array.

--- app.js
+++ app.js
@@ -2,6 +2,67 @@ console.log($('img[usemap]'));
 $('img[usemap]').maphilight()
 
 
+  console.log('working');
+
+
+
+
+  var game = {
+    player1: 'black',
+    player2: 'white',
+  }
+
+attackerRoll = [];
+defenderRoll = [];
+compareRoll = [];
+
+//compareRoll.push(Number(arr.splice(arr.indexOf(Math.max(...arr),1)).join())) //1. finds max of the array 2. joins and turns it into a string 3. turns it back into a number 4.pushes it to compareRoll array
+
+var max = function() {
+  //  for (var i = 0; i < attackerRoll.length; i++) {
+  //    if (attackerRoll[0] >= attackerRoll[1] && attackerRoll[0] > attackerRoll[2] || attackerRoll[0] >= attackerRoll[2] && attackerRoll[0] > attackerRoll[1]){
+  //      compareRoll.push($die1);
+  //    }
+  //    else if (attackerRoll[1] >= attackerRoll[0] && attackerRoll[1]>attackerRoll[2] || attackerRoll[1] >= attackerRoll[2] && attackerRoll[1] > attackerRoll[0]){
+  //      compareRoll.push($die2);
+  //    }
+  //    else {
+  //      compareRoll.push($die3);
+  //    }
+  //  }
+compareRoll.push(Number(attackerRoll.splice(attackerRoll.indexOf(Math.max(...attackerRoll)),1).join()))//1. finds max of the array 2. joins and turns it into a string 3. turns it back into a number 4.pushes it to compareRoll array
+console.log('max num',compareRoll)
+}
+
+
+
+  function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  var $die1 = randomNum(1, 6);
+  var $die2 = randomNum(1, 6);
+  var $die3 = randomNum(1, 6);
+  var $die4 = randomNum(1, 6);
+  var $die5 = randomNum(1, 6);
+
+  var $butt = $('#butt')
+
+  var $dice = $('#dice')
+
+  $butt.on('click', function(){
+  $dice.html("Attacker: " + $die1 + ",  " + $die2 + ",  " + $die3 + " " + " Defender: " + $die4 + ", " + $die5);
+  attackerRoll.push($die1,$die2,$die3);
+  defenderRoll.push($die4,$die5)
+  console.log(attackerRoll);
+  console.log(defenderRoll);
+  max();
+  max();
+  })
+
+
+
 
 
   var $continents = {
@@ -57,4 +118,11 @@ $('img[usemap]').maphilight()
     greenland: $('.greenland'),
     northwestTerritory: $('.northwestTerritory'),
     alaska: $('.alaska')
+  }
+
+  function switchTurns() {
+    currentPlayer = game.player1;
+    if (currentPlayer = gam.player){
+      currentPlayer = game.player2;
+    }
   }

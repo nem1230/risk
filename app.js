f67b99cd8879faf8fce039836606a6933a60370f
Nick N <n.nemetz1230@gmail.com>
2016-05-17 14:34:21 -0700

    set all boxes to height and width 0 so that there was more area map underneath that was available to be clicked.

--- app.js
+++ app.js
@@ -28,55 +28,100 @@ console.log($('img[usemap]'));
       $southAmerica: $('.southAmerica'),
       $northAmerica: $('.northAmerica')
     },
-    $territories: $('.countries'), //fisher yates
-    $territories_Obj: { $indonesia: $('.indonesia').first(),
-        $newGuinea: $('.newGuinea').first(),
-        $westernAus: $('.westernAus').first(),
-        $easternAus: $('.easternAus').first(),
-        $siam: $('.siam').first(),
-        $india: $('.india').first(),
-        $china: $('.china').first(),
-        $middleEast: $('.middleEast').first(),
-        $afghanistan: $('.afghanistan').first(),
-        $ural: $('.ural').first(),
-        $siberia: $('.siberia').first(),
-        $yakutsk: $('.yakutsk').first(),
-        $irkutsk: $('.irkutsk').first(),
-        $mongolia: $('.mongolia').first(),
-        $japan: $('.japan').first(),
-        $kamchatka: $('.kamchatka').first(),
-        $ukraine: $('.ukraine').first(),
-        $scandanavia: $('.scandanavia').first(),
-        $iceland: $('.iceland').first(),
-        $greatBritain: $('.greatBritain').first(),
-        $northernEurope: $('.northernEurope').first(),
-        $westernEurope: $('.westernEurope').first(),
-        $southernEurope: $('.southernEurope').first(),
-        $northAfrica: $('.northAfrica').first(),
-        $egypt: $('.egypt').first(),
-        $eastAfrica: $('.eastAfrica').first(),
-        $congo: $('.congo').first(),
-        $southAfrica: $('.southAfrica').first(),
-        $madagascar: $('.madagascar').first(),
-        $argentina: $('.argentina').first(),
-        $peru: $('.peru').first(),
-        $brazil: $('.brazil').first(),
-        $venezuela: $('.venezuela').first(),
-        $centralAmerica: $('.centralAmerica').first(),
-        $easternUnitedStates: $('.easternUnitedStates').first(),
-        $westernUnitedStates: $('.westernUnitedStates').first(),
-        $alberta: $('.alberta').first(),
-        $ontario: $('.ontario').first(),
-        $quebec: $('.quebec').first(),
-        $greenland: $('.greenland').first(),
-        $northwestTerritory: $('.northwestTerritory').first(),
-        $alaska: $('.alaska').first()
-        },
+    $territories: $('.terrriy_dis'), //fisher yates
+    // $territories_Obj_imageMap: { $indonesia: $('.indonesia').first(),
+    //     $newGuinea: $('.newGuinea').first(),
+    //     $westernAus: $('.westernAus').first(),
+    //     $easternAus: $('.easternAus').first(),
+    //     $siam: $('.siam').first(),
+    //     $india: $('.india').first(),
+    //     $china: $('.china').first(),
+    //     $middleEast: $('.middleEast').first(),
+    //     $afghanistan: $('.afghanistan').first(),
+    //     $ural: $('.ural').first(),
+    //     $siberia: $('.siberia').first(),
+    //     $yakutsk: $('.yakutsk').first(),
+    //     $irkutsk: $('.irkutsk').first(),
+    //     $mongolia: $('.mongolia').first(),
+    //     $japan: $('.japan').first(),
+    //     $kamchatka: $('.kamchatka').first(),
+    //     $ukraine: $('.ukraine').first(),
+    //     $scandanavia: $('.scandanavia').first(),
+    //     $iceland: $('.iceland').first(),
+    //     $greatBritain: $('.greatBritain').first(),
+    //     $northernEurope: $('.northernEurope').first(),
+    //     $westernEurope: $('.westernEurope').first(),
+    //     $southernEurope: $('.southernEurope').first(),
+    //     $northAfrica: $('.northAfrica').first(),
+    //     $egypt: $('.egypt').first(),
+    //     $eastAfrica: $('.eastAfrica').first(),
+    //     $congo: $('.congo').first(),
+    //     $southAfrica: $('.southAfrica').first(),
+    //     $madagascar: $('.madagascar').first(),
+    //     $argentina: $('.argentina').first(),
+    //     $peru: $('.peru').first(),
+    //     $brazil: $('.brazil').first(),
+    //     $venezuela: $('.venezuela').first(),
+    //     $centralAmerica: $('.centralAmerica').first(),
+    //     $easternUnitedStates: $('.easternUnitedStates').first(),
+    //     $westernUnitedStates: $('.westernUnitedStates').first(),
+    //     $alberta: $('.alberta').first(),
+    //     $ontario: $('.ontario').first(),
+    //     $quebec: $('.quebec').first(),
+    //     $greenland: $('.greenland').first(),
+    //     $northwestTerritory: $('.northwestTerritory').first(),
+    //     $alaska: $('.alaska').first()
+    //     },
+    $territories_Obj_div: {
+        $$indonesia: $('#indonesia_icon'),
+        $$newGuinea: $('#newGuinea_icon'),
+        $$westernAus: $('#westernAus_icon'),
+        $$easternAus: $('#easternAus_icon'),
+        $$siam: $('#siam_icon'),
+        $$india: $('#india_icon'),
+        $$china: $('#china_icon'),
+        $$middleEast: $('#middleEast_icon'),
+        $$afghanistan: $('#afghanistan_icon'),
+        $$ural: $('#ural_icon'),
+        $$siberia: $('#siberia_icon'),
+        $$yakutsk: $('#yakutsk_icon'),
+        $$irkutsk: $('#irkutsk_icon'),
+        $$mongolia: $('#mongolia_icon'),
+        $$japan: $('#japan_icon'),
+        $$kamchatka: $('#kamchatka_icon'),
+        $$ukraine: $('#ukraine_icon'),
+        $$scandanavia: $('#scandanavia_icon'),
+        $$iceland: $('#iceland_icon'),
+        $$greatBritain: $('#greatBritain_icon'),
+        $$northernEurope: $('#northernEurope_icon'),
+        $$westernEurope: $('#westernEurope_icon'),
+        $$southernEurope: $('#southernEurope_icon'),
+        $$northAfrica: $('#northAfrica_icon'),
+        $$egypt: $('#egypt_icon'),
+        $$eastAfrica: $('#eastAfrica_icon'),
+        $$congo: $('#congo_icon'),
+        $$southAfrica: $('#southAfrica_icon'),
+        $$madagascar: $('#madagascar_icon'),
+        $$argentina: $('#argentina_icon'),
+        $$peru: $('#peru_icon'),
+        $$brazil: $('#brazil_icon'),
+        $$venezuela: $('#venezuela_icon'),
+        $$centralAmerica: $('#centralAmerica_icon'),
+        $$easternUnitedStates: $('#easternUnitedStates_icon'),
+        $$westernUnitedStates: $("#westernUnitedStates_icon"),
+        $$alberta: $("#alberta_icon"),
+        $$ontario: $('#ontario_icon'),
+        $$quebec: $('#quebec_icon'),
+        $$greenland: $('#greenland_icon'),
+        $$northwestTerritory: $('#northwestTerritory_icon'),
+        $$alaska: $('#alaska_icon'),
+    },
       // $territories_Arr: [this.territories_Obj.$indonesia,$newGuinea,$westernAus,$easternAus,
       //   $siam,$india,$china,$middleEast,$afghanistan,$ural,$siberia,$yakutsk,$irkutsk,$mongolia,$japan,$kamchatka,
       //   $ukraine,$scandanavia,$iceland,$greatBritain,$northernEurope,$westernEurope,$southernEurope,
       //   $northAfrica,$egypt,$eastAfrica,$madagascar,$argentina,$peru,$brazil,$venezuela,
       //   $centralAmerica,$easternUnitedStates,$westernUnitedStates,$alberta,$ontario,$quebec,$greenland,$northwestTerritory,$alaska],
+      $displays: $('.display'),
       $start_game: $('#start_game'),
       $territories_Arr: [],
       $player1Territories: [],
@@ -98,19 +143,27 @@ console.log($('img[usemap]'));
 }
 }
 
+
+
   game.$start_game.on('click',function(){
-    for (territory in game.$territories_Obj){
-      game.$territories_Arr.push(game.$territories_Obj[territory]);
+    for (territory in game.$territories_Obj_div){
+      game.$territories_Arr.push(game.$territories_Obj_div[territory]);
+      console.log('territories array', game.$territories_Arr);
     }
     console.log('array exists')
     shuffle(game.$territories_Arr);
     console.log('array shuffled');
-    for (var i = 0; i<game.$territories_Arr.length; i++) {
-    game.$player1Territories.push(game.$territories_Arr);
+    game.$player1Territories= game.$territories_Arr.slice(0,22);
     console.log('player1', game.$player1Territories)
-  }
-  game.$player2Territories.push(game.$player1Territories.splice(0, 21));
-    console.log('player2', game.$player2Territories)
+    game.$player2Territories= game.$territories_Arr.slice(22,43);
+    console.log('player2', game.$player2Territories);
+
+    for (var i = 0; i < game.$player1Territories.length; i++) {
+        game.$player1Territories[i].css('color', 'white');
+    }
+    for (var i =0; i<  game.$player2Territories.length; i++){
+        game.$player2Territories[i].css('color', 'orange');
+    }
 })
 
 
@@ -162,7 +215,7 @@ console.log($('img[usemap]'));
   //     $alaska: $('.alaska')
   //   }
 
-  console.log('game.$territories_Obj',game.$territories_Obj)
+  // console.log('game.$territories_Obj_div',game.$territories_Obj_div)
   var $die1 = game.randomNum(1, 6);
   var $die2 = game.randomNum(1, 6);
   var $die3 = game.randomNum(1, 6);

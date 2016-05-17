3c2017e8d34324e139b60d2a333e334e278ab6d7
Nick N <n.nemetz1230@gmail.com>
2016-05-16 19:48:02 -0700

    trying to figure out ways to start the game. as of right now there are a series of arrays that are functioning to both divy up territories at the start of the game as well as store ownership. I couldn't display anything in an area tag so I made divs for each territory. looking forward to getting things displayed on the map now

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 console.log($('img[usemap]'));
-$('img[usemap]').maphilight()
+// $('img[usemap]').maphilight()
 
 
   console.log('working');
 
 
 
-
   var game = {
+    this: console.log('this', this),
     player1: 'black',
     player2: 'white',
     randomNum: function (min, max) {
@@ -16,15 +16,153 @@ $('img[usemap]').maphilight()
     $roll: $('#butt'),
     $dice: $('#dice'),
     max: function() {
-    compareRoll.push(Number(attackerRoll.splice(attackerRoll.indexOf(Math.max(...attackerRoll)),1).join()))//1. finds max of the array 2. joins and turns it into a string 3. turns it back into a number 4.pushes it to compareRoll array
-    console.log('max num',compareRoll)
+    game.compareRoll.push(Number(game.attackerRoll.splice(game.attackerRoll.indexOf(Math.max(...game.attackerRoll)),1).join()))//1. finds max of the array 2. joins and turns it into a string 3. turns it back into a number 4.pushes it to compareRoll array
+    console.log('max num', game.compareRoll)
     },
-
+    attackerRoll: [],
+    compareRoll: [],
+    $continents: {  $australia: $('.australia'),
+      $asia: $('.asia'),
+      $europe: $('.europe'),
+      $africa: $('.africa'),
+      $southAmerica: $('.southAmerica'),
+      $northAmerica: $('.northAmerica')
+    },
+    $territories: $('.countries'), //fisher yates
+    $territories_Obj: { $indonesia: $('.indonesia').first(),
+        $newGuinea: $('.newGuinea').first(),
+        $westernAus: $('.westernAus').first(),
+        $easternAus: $('.easternAus').first(),
+        $siam: $('.siam').first(),
+        $india: $('.india').first(),
+        $china: $('.china').first(),
+        $middleEast: $('.middleEast').first(),
+        $afghanistan: $('.afghanistan').first(),
+        $ural: $('.ural').first(),
+        $siberia: $('.siberia').first(),
+        $yakutsk: $('.yakutsk').first(),
+        $irkutsk: $('.irkutsk').first(),
+        $mongolia: $('.mongolia').first(),
+        $japan: $('.japan').first(),
+        $kamchatka: $('.kamchatka').first(),
+        $ukraine: $('.ukraine').first(),
+        $scandanavia: $('.scandanavia').first(),
+        $iceland: $('.iceland').first(),
+        $greatBritain: $('.greatBritain').first(),
+        $northernEurope: $('.northernEurope').first(),
+        $westernEurope: $('.westernEurope').first(),
+        $southernEurope: $('.southernEurope').first(),
+        $northAfrica: $('.northAfrica').first(),
+        $egypt: $('.egypt').first(),
+        $eastAfrica: $('.eastAfrica').first(),
+        $congo: $('.congo').first(),
+        $southAfrica: $('.southAfrica').first(),
+        $madagascar: $('.madagascar').first(),
+        $argentina: $('.argentina').first(),
+        $peru: $('.peru').first(),
+        $brazil: $('.brazil').first(),
+        $venezuela: $('.venezuela').first(),
+        $centralAmerica: $('.centralAmerica').first(),
+        $easternUnitedStates: $('.easternUnitedStates').first(),
+        $westernUnitedStates: $('.westernUnitedStates').first(),
+        $alberta: $('.alberta').first(),
+        $ontario: $('.ontario').first(),
+        $quebec: $('.quebec').first(),
+        $greenland: $('.greenland').first(),
+        $northwestTerritory: $('.northwestTerritory').first(),
+        $alaska: $('.alaska').first()
+        },
+      // $territories_Arr: [this.territories_Obj.$indonesia,$newGuinea,$westernAus,$easternAus,
+      //   $siam,$india,$china,$middleEast,$afghanistan,$ural,$siberia,$yakutsk,$irkutsk,$mongolia,$japan,$kamchatka,
+      //   $ukraine,$scandanavia,$iceland,$greatBritain,$northernEurope,$westernEurope,$southernEurope,
+      //   $northAfrica,$egypt,$eastAfrica,$madagascar,$argentina,$peru,$brazil,$venezuela,
+      //   $centralAmerica,$easternUnitedStates,$westernUnitedStates,$alberta,$ontario,$quebec,$greenland,$northwestTerritory,$alaska],
+      $start_game: $('#start_game'),
+      $territories_Arr: [],
+      $player1Territories: [],
+      $player2Territories: []
   }
 
-attackerRoll = [];
-compareRoll = [];
+  function shuffle(array) {
+   var m = array.length, t, i;
 
+// While there remain elements to shuffle…
+   while (m) {
+  // Pick a remaining element…
+  i = Math.floor(Math.random() * m--);
+
+  // And swap it with the current element.
+  t = array[m];
+  array[m] = array[i];
+  array[i] = t;
+}
+}
+
+  game.$start_game.on('click',function(){
+    for (territory in game.$territories_Obj){
+      game.$territories_Arr.push(game.$territories_Obj[territory]);
+    }
+    console.log('array exists')
+    shuffle(game.$territories_Arr);
+    console.log('array shuffled');
+    for (var i = 0; i<game.$territories_Arr.length; i++) {
+    game.$player1Territories.push(game.$territories_Arr);
+    console.log('player1', game.$player1Territories)
+  }
+  game.$player2Territories.push(game.$player1Territories.splice(0, 21));
+    console.log('player2', game.$player2Territories)
+})
+
+
+
+
+
+
+  // $territories_Obj: { $indonesia: $('.indonesia'),
+  //     $newGuinea: $('.newGuinea'),
+  //     $westernAus: $('.westernAus'),
+  //     $easternAus: $('.easternAus'),
+  //     $siam: $('.siam'),
+  //     $india: $('.india'),
+  //     $china: $('.china'),
+  //     $middleEast: $('.middleEast'),
+  //     $afghanistan: $('.afghanistan'),
+  //     $ural: $('.ural'),
+  //     $siberia: $('.siberia'),
+  //     $yakutsk: $('.yakutsk'),
+  //     $irkutsk: $('.irkutsk'),
+  //     $mongolia: $('.mongolia'),
+  //     $japan: $('.japan'),
+  //     $kamchatka: $('.kamchatka'),
+  //     $ukraine: $('.ukraine'),
+  //     $scandanavia: $('.scandanavia'),
+  //     $iceland: $('.iceland'),
+  //     $greatBritain: $('.greatBritain'),
+  //     $northernEurope: $('.northernEurope'),
+  //     $westernEurope: $('.westernEurope'),
+  //     $southernEurope: $('.southernEurope'),
+  //     $northAfrica: $('.northAfrica'),
+  //     $egypt: $('.egypt'),
+  //     $eastAfrica: $('.eastAfrica'),
+  //     $congo: $('.congo'),
+  //     $southAfrica: $('.southAfrica'),
+  //     $madagascar: $('.madagascar'),
+  //     $argentina: $('.argentina'),
+  //     $peru: $('.peru'),
+  //     $brazil: $('.brazil'),
+  //     $venezuela: $('.venezuela'),
+  //     $centralAmerica: $('.centralAmerica'),
+  //     $easternUnitedStates: $('.easternUnitedStates'),
+  //     $westernUnitedStates: $('.westernUnitedStates'),
+  //     $alberta: $('.alberta'),
+  //     $ontario: $('.ontario'),
+  //     $quebec: $('.quebec'),
+  //     $greenland: $('.greenland'),
+  //     $northwestTerritory: $('.northwestTerritory'),
+  //     $alaska: $('.alaska')
+  //   }
+
+  console.log('game.$territories_Obj',game.$territories_Obj)
   var $die1 = game.randomNum(1, 6);
   var $die2 = game.randomNum(1, 6);
   var $die3 = game.randomNum(1, 6);
@@ -33,79 +171,25 @@ compareRoll = [];
 
   game.$roll.on('click', function(){
   game.$dice.html("Attacker: " + $die1 + ",  " + $die2 + ",  " + $die3 + " " + " Defender: " + $die4 + ", " + $die5);
-  attackerRoll.push($die1,$die2,$die3);
-  compareRoll.push($die4,$die5)
-  console.log(attackerRoll);
+  game.attackerRoll.push($die1,$die2,$die3);
+  game.compareRoll.push($die4,$die5)
+  console.log(game.attackerRoll);
   game.max();
   game.max();
-  if (compareRoll[0] >= compareRoll[2] && compareRoll[1] >= compareRoll[3]) {
+  if (game.compareRoll[0] >= game.compareRoll[2] && game.compareRoll[1] >= game.compareRoll[3]) {
       console.log('-2 to the attacker');
   }
-  else if (compareRoll[0] >= compareRoll[2] && compareRoll[1] < compareRoll[3]) {
-    console.log('-1 to the attacker and -1 to the defender')
+  else if (game.compareRoll[0] >= game.compareRoll[2] && game.compareRoll[1] < game.compareRoll[3]) {
+      console.log('-1 to the attacker and -1 to the defender')
   }
-  else if (compareRoll[2] > compareRoll[0] && compareRoll[3] < compareRoll[1]) {
-    console.log('-1 to the defender and -1 to the attacker')
+  else if (game.compareRoll[2] > game.compareRoll[0] && game.compareRoll[3] < game.compareRoll[1]) {
+      console.log('-1 to the defender and -1 to the attacker')
   }
   else {
-    console.log('-2 to the defender')
+      console.log('-2 to the defender')
   }
   })
 
-  var $continents = {
-    $australia: $('.australia'),
-    $asia: $('.asia'),
-    $europe: $('.europe'),
-    $africa: $('.africa'),
-    $southAmerica: $('.southAmerica'),
-    $northAmerica: $('.northAmerica')
-  }
-
-
-  var territories = {
-    $indonesia: $('.indonesia'),
-    newGuinea: $('.newGuinea'),
-    westernAus: $('.westernAus'),
-    easternAus: $('.easternAus'),
-    siam: $('.siam'),
-    india: $('.india'),
-    china: $('.china'),
-    middleEast: $('.middleEast'),
-    afghanistan: $('.afghanistan'),
-    ural: $('.ural'),
-    siberia: $('.siberia'),
-    yakutsk: $('.yakutsk'),
-    irkutsk: $('.irkutsk'),
-    mongolia: $('.mongolia'),
-    japan: $('.japan'),
-    kamchatka: $('.kamchatka'),
-    ukraine: $('.ukraine'),
-    scandanavia: $('.scandanavia'),
-    iceland: $('.iceland'),
-    greatBritain: $('.greatBritain'),
-    northernEurope: $('.northernEurope'),
-    westernEurope: $('.westernEurope'),
-    southernEurope: $('.southernEurope'),
-    northAfrica: $('.northAfrica'),
-    egypt: $('.egypt'),
-    eastAfrica: $('.eastAfrica'),
-    congo: $('.congo'),
-    southAfrica: $('.southAfrica'),
-    madagascar: $('.madagascar'),
-    argentina: $('.argentina'),
-    peru: $('.peru'),
-    brazil: $('.brazil'),
-    venezuela: $('.venezuela'),
-    centralAmerica: $('.centralAmerica'),
-    easternUnitedStates: $('.easternUnitedStates'),
-    westernUnitedStates: $('.westernUnitedStates'),
-    alberta: $('.alberta'),
-    ontario: $('.ontario'),
-    quebec: $('.quebec'),
-    greenland: $('.greenland'),
-    northwestTerritory: $('.northwestTerritory'),
-    alaska: $('.alaska')
-  }
 
   function switchTurns() {
     currentPlayer = game.player1;
